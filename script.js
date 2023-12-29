61b04bb8d63f33fc080c45840a18081558508229
vladnnov <153611256+vladnnov@users.noreply.github.com>
2023-12-29 17:55:01 +0300

    Project complete
    Don't forget to look at browser console to check the output.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerWin = 0;
+let computerWin = 0;
+
 function getComputerChoice () {
     let computerChoice = Math.floor(Math.random() * 3);
     if(computerChoice === 0) {
@@ -9,3 +12,54 @@ function getComputerChoice () {
     }
     return computerChoice;
 }
+
+function playerSelection () {
+    let playerChoice = (prompt('Choose rock, paper or scissors')).toLowerCase();
+    return playerChoice;
+}
+
+function playSingleRound() {
+    let computerChoice = getComputerChoice();
+    let playerChoice = playerSelection();
+
+
+    if(computerChoice === 'rock' && playerChoice === 'rock') {
+        console.log('It\'s a draw!');
+    } else if (computerChoice === 'rock' && playerChoice === 'paper') {
+        playerWin++;
+        console.log('You win! Paper beats rock.')
+    } else if (computerChoice === 'rock' && playerChoice === 'scissors') {
+        computerWin++;
+        console.log('You lost! Rock beats scissors.')
+    } else if (computerChoice === 'paper' && playerChoice === 'paper') {
+        console.log('It\'s a draw!');
+    } else if (computerChoice === 'paper' && playerChoice === 'rock') {
+        computerWin++;
+        console.log('You lost! Paper beats rock.')
+    } else if (computerChoice === 'paper' && playerChoice === 'scissors') {
+        playerWin++;
+        console.log('You win! Scissors beat paper.')
+    } else if (computerChoice === 'scissors' && playerChoice === 'scissors') {
+        console.log('It\'s a draw!');
+    } else if (computerChoice === 'scissors' && playerChoice === 'rock') {
+        playerWin++;
+        console.log('You win! Rock beats scissors.')
+    } else if (computerChoice === 'scissors' && playerChoice === 'paper') {
+        computerWin++;
+        console.log('You lost! Scissors beat paper.')
+    }
+}
+
+function game() {
+    while(playerWin < 5 && computerWin < 5) {
+        playSingleRound();
+
+        if (playerWin === 5) {
+            console.log(`Well done! You win. Your score is ${playerWin} and computer score is ${computerWin}.`);
+        } else if (computerWin === 5) {
+            console.log(`Unfortunately you lost! Your score is ${playerWin} and computer score is ${computerWin}.`);
+        }
+    }
+}
+
+game();
